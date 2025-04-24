0ffc1d4bbbc1b1900d68310fa33199a2efb34f6e
Shawnade <deshawnagalloway@yahoo.com>
2025-04-24 15:29:18 -0400

    CARMEN SANDIEGO - I found Carmen Sandiego!

--- find_carmen (1).sql
+++ find_carmen (1).sql
@@ -93,10 +93,27 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
+-- SELECT name, countrycode FROM city WHERE city.name LIKE '%Serra%';
+--          name         | countrycode
+-- ----------------------+-------------
+--  Serra                | BRA   *** The answer is Serra ***
+--  Taboï¿½o da Serra    | BRA
+--  Itapecerica da Serra | BRA
+--  Serravalle           | SMR
+--  SELECT name FROM country WHERE country.code = 'BRA';
+--   name
+-- --------
+--  Brazil    *** In Barzil ***
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
-
+--  SELECT name, capital FROM country WHERE country.code = 'BRA';
+--   name  | capital
+-- --------+---------
+--  Brazil |     211
+--  SELECT name FROM city WHERE city.id = 211;
+--     name
+-- ------------
+--  Brasï¿½lia
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
@@ -112,5 +129,8 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
-
+-- SELECT * FROM city WHERE city.population = 91084;
+--   id  |     name     | countrycode |  district  | population
+-- ------+--------------+-------------+------------+------------
+--  4060 | Santa Monica | USA         | California |      91084    *** The answer is Santa Monica, CA ***
 

dcb153cdbe5b2364c69254797417af5edc896338
Shawnade <deshawnagalloway@yahoo.com>
2025-04-24 19:07:55 -0400

    completed realty_schema.sql

---
+++ realty_schema.sql
@@ -0,0 +1,50 @@
+-- enter your schema for apartments, offices and storefronts below
+-- realty_db=# CREATE TABLE apartments (id SERIAL PRIMARY KEY, apartment_number INT, bedrooms INT, bathrooms INT, address VARCHAR(50), tenant VARCHAR(35), occupied BOOL, sq_ft INT, price INT);
+--  Table "public.apartments"
+--       Column      |         Type          | Collation | Nullable |                Default             
+-- ------------------+-----------------------+-----------+----------+----------------------------------------
+--  id               | integer               |           | not null | nextval('apartments_id_seq'::regclass)
+--  apartment_number | integer               |           |          |
+--  bedrooms         | integer               |           |          |
+--  bathrooms        | integer               |           |          |
+--  address          | character varying(50) |           |          |
+--  tenant           | character varying(35) |           |          |
+--  occupied         | boolean               |           |          |
+--  sq_ft            | integer               |           |          |
+--  price            | integer               |           |          |
+-- Indexes:
+--     "apartments_pkey" PRIMARY KEY, btree (id)
+
+-- realty_db=# CREATE TABLE office (id SERIAL PRIMARY KEY, office_number INT, floors INT, sq_ft INT, cubicles INT, bedrooms INT, address VARCHAR(50), company VARCHAR, occupied BOOL, price INT);
+--   Table "public.office"
+--     Column     |         Type          | Collation | Nullable |              Default
+-- ---------------+-----------------------+-----------+----------+------------------------------------
+--  id            | integer               |           | not null | nextval('office_id_seq'::regclass)
+--  office_number | integer               |           |          |
+--  floors        | integer               |           |          |
+--  sq_ft         | integer               |           |          |
+--  cubicles      | integer               |           |          |
+--  bedrooms      | integer               |           |          |
+--  address       | character varying(50) |           |          |
+--  company       | character varying     |           |          |
+--  occupied      | boolean               |           |          |
+--  price         | integer               |           |          |
+-- Indexes:
+--     "office_pkey" PRIMARY KEY, btree (id)
+
+-- realty_db=# CREATE TABLE storefront (address VARCHAR(50), occupied BOOL, price INT, kitchen BOOL, sq_ft INT, owner VARCHAR, outdoor_seating BOOL);
+--   Table "public.office"
+--     Column     |         Type          | Collation | Nullable |              Default
+-- ---------------+-----------------------+-----------+----------+------------------------------------
+--  id            | integer               |           | not null | nextval('office_id_seq'::regclass)
+--  office_number | integer               |           |          |
+--  floors        | integer               |           |          |
+--  sq_ft         | integer               |           |          |
+--  cubicles      | integer               |           |          |
+--  bedrooms      | integer               |           |          |
+--  address       | character varying(50) |           |          |
+--  company       | character varying     |           |          |
+--  occupied      | boolean               |           |          |
+--  price         | integer               |           |          |
+-- Indexes:
+--     "office_pkey" PRIMARY KEY, btree (id)

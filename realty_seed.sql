36827c186509951b3352837db90adc5ecb9c0544
Shawnade <deshawnagalloway@yahoo.com>
2025-04-24 22:19:18 -0400

    completed realty_seed.sql

--- realty_seed.sql
+++ realty_seed.sql
@@ -6,6 +6,10 @@ INSERT INTO apartments (apartment_number, bedrooms, bathrooms, address, tenant, 
 INSERT INTO apartments (apartment_number, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ('516', '2', '2', '203 Crestview Blvd', 'Trish', 'true', '3500', '2600');
 
 
-INSERT INTO office (office_number, floors, sq_ft, cubicles, bathrooms, address, company, occupied, price) Values ('211', '3', '9000', '300','4', '1111 Center Street', 'None', 'false', '7000');
-INSERT INTO office (office_number, floors, sq_ft, cubicles, bathrooms, address, company, occupied, price) Values (')
-INSERT INTO office (office_number, floors, sq_ft, cubicles, bathrooms, address, company, occupied, price) Values ()
+INSERT INTO office (office_number, floors, sq_ft, cubicles, bathrooms, address, company, occupied, price) Values ('211', '3', '9000', '300','4', '1111 Center Street', '', 'false', '7000');
+INSERT INTO office (office_number, floors, sq_ft, cubicles, bathrooms, address, company, occupied, price) Values ('4827', '7', '70000', '2100', '21', '2408 Studeville Drive', 'Get Up & Go', 'true', '30000');
+INSERT INTO office (office_number, floors, sq_ft, cubicles, bathrooms, address, company, occupied, price) Values ('30', '2', '6000', '250', '5', '3 Midtown Place', 'Shoe & Shu Law', 'true', '5500');
+
+INSERT INTO storefront (address, occupied, price, kitchen, sq_ft, owner, outdoor_seating) VALUES ('3684 Sweets Ave', 'true', '6000', 'true', '6006', 'Crusty Treats Bakery Cafe', 'true');
+INSERT INTO storefront (address, occupied, price, kitchen, sq_ft, owner, outdoor_seating) VALUES ('78 Farmer Lane', 'true', '4500', 'false', '4550', 'Fruits And Veggies', 'false');
+INSERT INTO storefront (address, occupied, price, kitchen, sq_ft, owner, outdoor_seating) VALUES ('657 Front Street', 'false', '10000','true', '11000', 'Wine & Dine INC', 'true');

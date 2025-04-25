bb18984d3bdd0922aa47e58e2748500df3bf91cb
Shawnade <deshawnagalloway@yahoo.com>
2025-04-24 21:47:32 -0400

    redid realty to correctly follow instructions

--- realty_schema.sql
+++ realty_schema.sql
@@ -1,4 +1,43 @@
 -- enter your schema for apartments, offices and storefronts below
+drop table if exists apartments;
+drop table if exists office;
+drop table if exists storefront;
+
+create table apartments(
+    id serial primary key,
+    apartment_number integer,
+    bedrooms integer,
+    bathrooms integer,
+    address varchar(50),
+    tenant varchar(35),
+    occupied boolean,
+    sq_ft integer,
+    price integer
+);
+
+create table office(
+    id serial primary key,
+    office_number integer,
+    floors integer,
+    sq_ft integer,
+    cubicles integer,
+    bathrooms integer,
+    address varchar(50),
+    company varchar(30),
+    occupied boolean,
+    price integer
+);
+
+create table storefront(
+    address varchar(50),
+    occupied boolean,
+    price integer,
+    kitchen boolean,
+    sq_ft integer,
+    owner varchar(30),
+    outdoor_seating boolean
+);
+
 -- realty_db=# CREATE TABLE apartments (id SERIAL PRIMARY KEY, apartment_number INT, bedrooms INT, bathrooms INT, address VARCHAR(50), tenant VARCHAR(35), occupied BOOL, sq_ft INT, price INT);
 --  Table "public.apartments"
 --       Column      |         Type          | Collation | Nullable |                Default             
@@ -16,7 +55,7 @@
 --     "apartments_pkey" PRIMARY KEY, btree (id)
 
 -- realty_db=# CREATE TABLE office (id SERIAL PRIMARY KEY, office_number INT, floors INT, sq_ft INT, cubicles INT, bedrooms INT, address VARCHAR(50), company VARCHAR, occupied BOOL, price INT);
---   Table "public.office"
+--    Table "public.office"
 --     Column     |         Type          | Collation | Nullable |              Default
 -- ---------------+-----------------------+-----------+----------+------------------------------------
 --  id            | integer               |           | not null | nextval('office_id_seq'::regclass)
@@ -24,24 +63,7 @@
 --  floors        | integer               |           |          |
 --  sq_ft         | integer               |           |          |
 --  cubicles      | integer               |           |          |
---  bedrooms      | integer               |           |          |
---  address       | character varying(50) |           |          |
---  company       | character varying     |           |          |
---  occupied      | boolean               |           |          |
---  price         | integer               |           |          |
--- Indexes:
---     "office_pkey" PRIMARY KEY, btree (id)
-
--- realty_db=# CREATE TABLE storefront (address VARCHAR(50), occupied BOOL, price INT, kitchen BOOL, sq_ft INT, owner VARCHAR, outdoor_seating BOOL);
---   Table "public.office"
---     Column     |         Type          | Collation | Nullable |              Default
--- ---------------+-----------------------+-----------+----------+------------------------------------
---  id            | integer               |           | not null | nextval('office_id_seq'::regclass)
---  office_number | integer               |           |          |
---  floors        | integer               |           |          |
---  sq_ft         | integer               |           |          |
---  cubicles      | integer               |           |          |
---  bedrooms      | integer               |           |          |
+--  bathrooms     | integer               |           |          |
 --  address       | character varying(50) |           |          |
 --  company       | character varying     |           |          |
 --  occupied      | boolean               |           |          |
--- realty_seed.sql
+++ realty_seed.sql
@@ -1,1 +1,10 @@
 --enter your seed data below
+-- INSERT INTO apartments (apartment_number, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ('3203', '1', '1', '5708 Sunny Lane', 'Al Alone', 'true', '1700', '1300');
+-- INSERT INTO apartments (apartment_number, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ('679', '4', '3', '6898 Truly Drive', 'None', 'false', '6000', '5300');
+-- INSERT INTO apartments (apartment_number, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ('516', '2', '2', '203 Crestview Blvd', 'Trish', 'true', '3500', '2600');
+--  id | apartment_number | bedrooms | bathrooms |      address       |  tenant  | occupied | sq_ft | price
+-- ----+------------------+----------+-----------+--------------------+----------+----------+-------+-------
+--   1 |             3203 |        1 |         1 | 5708 Sunny Lane    | Al Alone | t        |  1700 |  1300
+--   2 |              679 |        4 |         3 | 6898 Truly Drive   | None     | f        |  6000 |  5300
+--   3 |              516 |        2 |         2 | 203 Crestview Blvd | Trish    | t        |  3500 |  2600
+

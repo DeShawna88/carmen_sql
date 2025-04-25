f06d502722ede99bc1529a701f62b1e2e7ace54c
Shawnade <deshawnagalloway@yahoo.com>
2025-04-24 21:49:29 -0400

    redid realty to correctly follow instructions

--- realty_seed.sql
+++ realty_seed.sql
@@ -1,10 +1,11 @@
 --enter your seed data below
--- INSERT INTO apartments (apartment_number, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ('3203', '1', '1', '5708 Sunny Lane', 'Al Alone', 'true', '1700', '1300');
--- INSERT INTO apartments (apartment_number, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ('679', '4', '3', '6898 Truly Drive', 'None', 'false', '6000', '5300');
--- INSERT INTO apartments (apartment_number, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ('516', '2', '2', '203 Crestview Blvd', 'Trish', 'true', '3500', '2600');
---  id | apartment_number | bedrooms | bathrooms |      address       |  tenant  | occupied | sq_ft | price
--- ----+------------------+----------+-----------+--------------------+----------+----------+-------+-------
---   1 |             3203 |        1 |         1 | 5708 Sunny Lane    | Al Alone | t        |  1700 |  1300
---   2 |              679 |        4 |         3 | 6898 Truly Drive   | None     | f        |  6000 |  5300
---   3 |              516 |        2 |         2 | 203 Crestview Blvd | Trish    | t        |  3500 |  2600
 
+
+INSERT INTO apartments (apartment_number, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ('3203', '1', '1', '5708 Sunny Lane', 'Al Alone', 'true', '1700', '1300');
+INSERT INTO apartments (apartment_number, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ('679', '4', '3', '6898 Truly Drive', 'None', 'false', '6000', '5300');
+INSERT INTO apartments (apartment_number, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ('516', '2', '2', '203 Crestview Blvd', 'Trish', 'true', '3500', '2600');
+
+
+INSERT INTO office (office_number, floors, sq_ft, cubicles, bathrooms, address, company, occupied, price) Values ('211', '3', '9000', '300','4', '1111 Center Street', 'None', 'false', '7000');
+INSERT INTO office (office_number, floors, sq_ft, cubicles, bathrooms, address, company, occupied, price) Values (')
+INSERT INTO office (office_number, floors, sq_ft, cubicles, bathrooms, address, company, occupied, price) Values ()
